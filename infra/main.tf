4e659a79e507b9466ef34b0bc1862c9070cf9fd5
tjn777 <tn315@sussex.ac.uk>
2026-01-16 18:21:14 +0000

    Initial infrastructure, Lambda API, and EC2 service structure

---
+++ infra/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+
+
+resource "aws_dynamodb_table" "users" {
+  name         = "${var.project_name}-users"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "user_id"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "${var.project_name}-users"
+  }
+}
